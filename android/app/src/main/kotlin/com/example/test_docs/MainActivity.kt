015ab8c29c4bfbfb77b58545486b1d8fde1d2bc7
Oeun-Chenda <alexchenda.rupp@gmail.com>
2022-10-22 17:06:23 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/test_docs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_docs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home/models/product.dart
@@ -0,0 +1,29 @@
+class Product {
+  String? name;
+  double? price;
+  String? image;
+
+  Product({
+    this.image,
+    this.name,
+    this.price,
+  });
+}
+
+List productList = [
+  Product(
+    name: "Sofa",
+    image: "url",
+    price: 200.0,
+  ),
+  Product(
+    name: "Sofa",
+    image: "url",
+    price: 200.0,
+  ),
+  Product(
+    name: "Sofa",
+    image: "url",
+    price: 200.0,
+  ),
+];
